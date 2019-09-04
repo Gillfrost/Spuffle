504515217e4da988db8024ac93d80e5bcc3dd5db
André Gillfrost <28143870+Gillfrost@users.noreply.github.com>
2019-09-04 06:07:27 +0200

    Add sign in button

---
+++ Spuffle/SignInViewController.swift
@@ -0,0 +1,11 @@
+//  Copyright (c) 2019 André Gillfrost
+//  Licensed under the MIT license
+
+import UIKit
+
+final class SignInViewController: UIViewController {
+
+    @IBAction private func signIn() {
+        print(#function)
+    }
+}
