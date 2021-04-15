5be9174e94a9f35e0a12548319e2779bd04f0618
André Gillfrost <28143870+Gillfrost@users.noreply.github.com>
2021-04-15 06:37:36 +0200

    Add Spotify player test for error handling when requesting user

--- SpuffleTests/SpotifyPlayerTests.swift
+++ SpuffleTests/SpotifyPlayerTests.swift
@@ -28,12 +28,13 @@ final class SpotifyPlayerTests: XCTestCase {
         waitForExpectations(timeout: 0)
     }
 
-    func testSetup_retryAfterError() {
+    func testSetup_retryAfterErrorInStartRequest() {
 
         let errorExpectation = expectation(description: "error")
         let loadingExpectation = expectation(description: "loading")
 
-        let startResults = futureFactory(of: .failure(error), .success(()))
+        let startResults = futureFactory(.failure(error),
+                                         .success(()))
 
         let mock = SpotifyMock(start: { _ in startResults() })
 
@@ -57,12 +58,43 @@ final class SpotifyPlayerTests: XCTestCase {
         wait(for: [errorExpectation, loadingExpectation], timeout: 0, enforceOrder: true)
     }
 
-    func testLoadPlaylists_retryAfterError() {
+    func testLoading_retryAfterErrorInUserRequest() {
 
         let errorExpectation = expectation(description: "error")
         let loadedExpectation = expectation(description: "loaded")
 
-        let playlistsResults = futureFactory(of: .failure(error),
+        let requestCurrentUserResults = futureFactory(.failure(error),
+                                                      .success(SPTUser()))
+
+        let mock = SpotifyMock(
+            requestCurrentUser: { _ in requestCurrentUserResults() })
+
+        player(spotify: mock)
+            .state
+            .dropFirst()
+            .prefix(2)
+            .sink { state in
+                switch state {
+                case .error(_, let retry):
+                    errorExpectation.fulfill()
+                    retry()
+                case .loaded:
+                    loadedExpectation.fulfill()
+                default:
+                    XCTFail(state.description)
+                }
+            }
+            .store(in: &cancellables)
+
+        wait(for: [errorExpectation, loadedExpectation], timeout: 1, enforceOrder: true)
+    }
+
+    func testLoading_retryAfterErrorInPlaylistsRequest() {
+
+        let errorExpectation = expectation(description: "error")
+        let loadedExpectation = expectation(description: "loaded")
+
+        let playlistsResults = futureFactory(.failure(error),
                                              .success([SPTPartialPlaylist]()))
 
         let mock = SpotifyMock(
@@ -99,15 +131,15 @@ private extension SpotifyPlayerTests {
         SpotifyPlayer(spotify: spotify, token: token)
     }
 
-    func futureFactory<Output, Failure>(of results: Result<Output, Failure>...) -> () -> Future<Output, Failure> {
+    func futureFactory<Output, Failure>(_ sequentialResults: Result<Output, Failure>...) -> () -> Future<Output, Failure> {
         var count = 0
         return {
             defer { count += 1 }
             return Future { promise in
-                guard count < results.count else {
+                guard count < sequentialResults.count else {
                     fatalError("More results demanded than were supplied")
                 }
-                promise(results[count])
+                promise(sequentialResults[count])
             }
         }
     }
